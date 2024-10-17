0eab7fe0a9cc4a391ce87ea361045f5ad4d08de3
DarthScaburous <cegbert2@uwyo.edu>
2024-10-17 23:10:26 +0000

    done

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Caleb Egbert
 # UWYO COSC 1010
-# Submission Date
+# October 17, 2024
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 12
+# Sources, people worked with, help given to: TA, Mak Weinzierl
 # your
 # comments
 # here
@@ -63,6 +63,9 @@ zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
 """
 random_string = random_string.replace("\n","") #remove all newline characters
 print(len(random_string)) # Print out the size for reference 
+# key:value
+
+
 
 # Above is a string with 2500 characters.
 # Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
@@ -84,17 +87,42 @@ print(len(random_string)) # Print out the size for reference
 #Will need to first declare a dictionary 
 
 # Output: each letter and its corresponding occurrence in alphabetical order
+letter_count = {}
+for letter in sorted(random_string):
+    if letter not in letter_count.keys():
+        letter_count[letter] = 1
+    else:
+        letter_count[letter] += 1
+    
+
+print(letter_count)
+
 
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+most_occurred = "k"
+least_occurred = "n"
 
+sorted_dict = sorted(letter_count.values())
+
+for numbers in letter_count:
+    if letter_count[numbers] == sorted_dict[-1]:
+        most_occured = numbers
+
+for numbers in letter_count:
+    if letter_count[numbers] == sorted_dict[0]:
+        least_occured = numbers
+
+        
+
+    
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+print(f"The letter that occurred the least is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+for letters in letter_count:
+    print(letters,letter_count[letters]/len(random_string)*100)
